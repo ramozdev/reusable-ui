5e2ae22dfbcdb1c4285d96d4c5e63e6bd7f01c6e
robinglto <robinsomedia@pm.me>
2023-11-29 17:13:23 -0400

    add nav

--- src/app/uniswap/page.tsx
+++ src/app/uniswap/page.tsx
@@ -1,9 +1,13 @@
 import Swap from "@/components/swap";
+import Nav from "@/components/nav";
 
 export default function Page() {
   return (
-    <div className="mx-auto grid h-screen w-full items-center justify-center">
-      <Swap />
+    <div>
+      <Nav />
+      <div className="mx-auto grid h-screen w-full items-center justify-center">
+        <Swap />
+      </div>
     </div>
   );
 }
