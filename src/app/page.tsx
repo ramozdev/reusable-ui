9ecfb6ab4af193daf4c0e53ac18f4e401ad2a456
Diego Ramos <ramosdiego@pm.me>
2023-12-12 14:02:25 -0400

    add dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,14 @@ import Buttons from "@/components/hero/buttons";
 import SelectDemo from "@/components/homepage/select";
 import ContextMenuDemo from "@/components/homepage/context-menu";
 import DropdownMenuDemo from "@/components/homepage/dropdown-menu";
+import DialogDemo from "@/components/homepage/dialog";
 
 export default function Page() {
   return (
     <div className="mx-auto my-12 max-w-screen-xl space-y-8">
       <ContextMenuDemo />
       <DropdownMenuDemo />
+      <DialogDemo />
       <InputDemo />
       <TextareaDemo />
       <CheckboxDemo />
---
+++ src/components/homepage/dialog.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Dialog from "@/ui/radix/dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import Button from "@/ui/html/button";
+import Input from "@/ui/html/input";
+import Text from "@/ui/html/text";
+
+export default function DialogDemo() {
+  return (
+    <div>
+      <Text.H3>Dialog</Text.H3>
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <Button.Solid color="neutral">Edit profile</Button.Solid>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay />
+          <Dialog.Content>
+            <Dialog.Title>Edit profile</Dialog.Title>
+            <Dialog.Description>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </Dialog.Description>
+            <fieldset className="mb-[15px] flex items-center gap-5">
+              <label htmlFor="name">Name</label>
+              <Input id="name" defaultValue="Columpio Labs" />
+            </fieldset>
+            <fieldset className="mb-[15px] flex items-center gap-5">
+              <label htmlFor="username">Username</label>
+              <Input id="username" defaultValue="@ColumpioLabs" />
+            </fieldset>
+            <div className="mt-[25px] flex justify-end">
+              <Dialog.Close asChild>
+                <Button.Subtle color="neutral">Save changes</Button.Subtle>
+              </Dialog.Close>
+            </div>
+            <Dialog.Close asChild>
+              <Button.Subtle
+                size="icon"
+                color="neutral"
+                className="absolute right-[10px] top-[10px]"
+                aria-label="Close"
+              >
+                <Cross2Icon />
+              </Button.Subtle>
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </div>
+  );
+}
